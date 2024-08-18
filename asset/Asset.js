0c4241362adf1db6fe5bed58ea6111d29faf8239
Md. Kabirul Hossain <mdkabirulhossainj@gmail.com>
2024-08-19 00:03:43 +0600

    Asset are Updated

---
+++ asset/Asset.js
@@ -0,0 +1,155 @@
+import blog_img_01 from './blog-01.jpg'
+import blog_img_02 from './blog-02.jpg'
+import blog_img_03 from './blog-3.jpg'
+import blog_img_04 from './blog-4.jpg'
+import blog_img_05 from './blog-5.jpg'
+import blog_img_06 from './blog-06.jpg'
+import blog_img_07 from './blog-07.jpg'
+import blog_img_08 from './blog-8.jpg'
+import blog_img_09 from './blog-09.jpg'
+import blog_img_10 from './blog-10.jpg'
+import blog_img_11 from './blog-11.png'
+import blog_img_12 from './blog-12.png'
+import blog_img_13 from './blog-13.png'
+import blog_img_14 from './blog-14.png'
+import blog_img_15 from './blog-15.png'
+
+export const blog_data =[
+    {
+        id:1,
+        title: "Wired",
+        description: "Wired is a massively popular publication, good at capturing real insights into the tech world, no stranger to topics like technology, entertainment, science, culture, politics, and social media. Informed and comprehensive, Wired is basically the perfect tech blog to follow.",
+        image: blog_img_01,
+        date: Date.now(),
+        category:"Technology",
+        Author: "Louis Rossetto, Jane Metcalfe",
+    },
+    {
+        id:2,
+        title: "TechCrunch",
+        description: "Invest yourself in the business verse of the tech world with one of the most popular technology blogs thriving this year- TechCrunch.",
+        image: blog_img_02,
+        date: Date.now(),
+        category:"Technology",
+        Author: "Michael Arrington, Keith Teare",
+    },
+    {
+        id:3,
+        title: " Recode",
+        description: "Recode is uncovering and explaining how our tech world is changing by focusing on the businesses of Silicon Valley.",
+        image: blog_img_03,
+        date: Date.now(),
+        category:"Technology",
+        Author: "Kara Swisher",
+    },
+    {
+        id:4,
+        title: " Mashable",
+        description: "Powered by its own proprietary technology, this tech blog is the go-to source for tech, digital culture, and entertainment content for its influential and dedicated global audience!",
+        image: blog_img_04,
+        date: Date.now(),
+        category:"Technology",
+        Author: "Pete Cashmoreg",
+    },
+    {
+        id:5,
+        title: " CNET",
+        description: "CNET stays ahead of the curve by spreading its hands on all sorts of incredible tech products, and trends and offering unbiased reviews.",
+        image: blog_img_05,
+        date: Date.now(),
+        category:"Technology",
+        Author: "Halsey Minor, Shelby Bonnie",
+    },
+    {
+        id:6,
+        title: " Startupbootcamp Blog",
+        description: "Startupbootcamp is a global network of industry-focused startup accelerators, supporting early-stage tech founders to rapidly scale their companies by providing direct access to an international network of mentors, partners, and investors.",
+        image: blog_img_06,
+        date: Date.now(),
+        category:"StartUp",
+        Author: "Sheikh Rakin",
+    },
+    {
+        id:7,
+        title: "StartUs Magazine",
+        description: "Startupbootcamp is a global network of industry-focused startup accelerators, supporting early-stage tech founders to rapidly scale their companies by providing direct access to an international network of mentors, partners, and investors.",
+        image: blog_img_07,
+        date: Date.now(),
+        category:"StartUp",
+        Author: "Umar Faruk",
+    },
+    {
+        id:8,
+        title: "StartUs Magazine",
+        description: "Startupbootcamp is a global network of industry-focused startup accelerators, supporting early-stage tech founders to rapidly scale their companies by providing direct access to an international network of mentors, partners, and investors.",
+        image: blog_img_08,
+        date: Date.now(),
+        category:"StartUp",
+        Author: "Md. Kabirul Hossain",
+    },
+    {
+        id:9,
+        title: "StartupNation",
+        description: "StartupNation is a comprehensive platform offering resources, advice, and community support for entrepreneurs. Founded by Jeff and Rich Sloan, the blog provides a wealth of information on business planning.",
+        image: blog_img_09,
+        date: Date.now(),
+        category:"StartUp",
+        Author: "Md. Abul",
+    },
+    {
+        id:10,
+        title: "Startup Daily",
+        description: "Startup Daily is a leading publication that covers the dynamic tech ecosystem in Australia and New Zealand.",
+        image: blog_img_10,
+        date: Date.now(),
+        category:"StartUp",
+        Author: "Md. Harun",
+    },
+    {
+        id:11,
+        title: "Cup of jo",
+        description: "Cup of Jo presents a clean, modern design that’s easy to navigate and visually appealing.",
+        image: blog_img_11,
+        date: Date.now(),
+        category:"Lifestyle",
+        Author: "Md. Biplob",
+    },
+    {
+        id:12,
+        title: "The Chriselle Factor",
+        description: "The website has a stylish, modern look that shows off Chriselle Lim’s knowledge of fashion and beauty.",
+        image: blog_img_12,
+        date: Date.now(),
+        category:"Lifestyle",
+        Author: "Md. Hown",
+    },
+    {
+        id:13,
+        title: "The Skinny Confidential",
+        description: "The blog has a fashionable, vibrant feel with its stylish design and signature pink color scheme.",
+        image: blog_img_13,
+        date: Date.now(),
+        category:"Lifestyle",
+        Author: "Md. Hown",
+    },
+    {
+        id:14,
+        title: "Ape To Gentleman",
+        description: "The blog has a fashionable, vibrant feel with its stylish design and signature pink color scheme.",
+        image: blog_img_14,
+        date: Date.now(),
+        category:"Lifestyle",
+        Author: "Md. Hown",
+    },
+    {
+        id:15,
+        title: "Camille Styles",
+        description: "The blog has a fashionable, vibrant feel with its stylish design and signature pink color scheme.",
+        image: blog_img_15,
+        date: Date.now(),
+        category:"Lifestyle",
+        Author: "Md. Hown",
+    },
+
+
+]
