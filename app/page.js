7e8126ee0998e9a4465f6b8aa56d2c010ee1fc0d
Md. Kabirul Hossain <mdkabirulhossainj@gmail.com>
2024-08-19 15:37:11 +0600

    Footer componet called in root page

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Header from "./components/Header";
 import BLogList from "./components/BLogList";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <div >
       <Header></Header>
       <BLogList></BLogList>
+      <Footer></Footer>
     </div>
   );
 }
